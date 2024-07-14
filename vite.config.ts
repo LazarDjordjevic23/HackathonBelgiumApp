5aa4f3e2d354505f92b1621288dc192488a29daf
LazarDjordjevic23 <djordjeviclazar12@gmail.com>
2024-07-14 04:25:15 +0200

    setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  server: {
+    fs: {
+      cachedChecks: false,
+    },
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, './src') },
+      { find: 'public', replacement: resolve(__dirname, './public') }
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {},
+    },
+  },
+});
